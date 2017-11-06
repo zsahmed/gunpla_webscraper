eba4484f2c72b25428a3f640d0645ca5e6d5aee9
Zakariya Ahmed <zahmed@credera.com>
2017-11-05 21:38:24 -0600

    added try catch so error won't stop creation on json results

--- mg.py
+++ mg.py
@@ -23,45 +23,48 @@ for year_release in range(1995, 2018):
             except IndexError:
                 image = "No image available"
             dictionary['image'] = image
-    
-        elif(i % 6 == 1):         
+
+        elif(i % 6 == 1):
             title = tds[i].text
             link = tds[i].find_element_by_tag_name('a').get_attribute("href")
             print "title: " + title
             print "link: " + link
             dictionary['title'] = title
             dictionary['link'] = link
-            
-        elif(i % 6 == 2):   
+
+        elif(i % 6 == 2):
             series = tds[i].find_element_by_css_selector('a').get_attribute("title")
             dictionary['series'] = series
-            
+
         elif(i % 6 == 3):
             price = tds[i].text
             dictionary['price'] = price
-            
+
         elif(i % 6 == 4):
             release_date = tds[i].text
             dictionary['release_date'] = release_date
             kits.append(dictionary)
             dictionary = {}
-print "=================================="    
+print "=================================="
 browser.close()
 print kits
 
 for index, kit in enumerate(kits):
-    page_to_visit = kit['link']
-    print "connecting to ", page_to_visit
-    minibrowser = webdriver.PhantomJS()
-    minibrowser.get(page_to_visit)
-    print "model name: ", kit['title']
-    info_text =  minibrowser.find_elements_by_css_selector("div.mw-content-text > p")[0].text
-    print "info_text: ", info_text
-    kit['info_text'] = info_text
-    print len(kits)-index-1, " remaining..."
-    minibrowser.close()
+    try:
+        page_to_visit = kit['link']
+        print "connecting to ", page_to_visit
+        minibrowser = webdriver.PhantomJS()
+        minibrowser.get(page_to_visit)
+        print "model name: ", kit['title']
+        info_text =  minibrowser.find_elements_by_css_selector("div.mw-content-text > p")[0].text
+        print "info_text: ", info_text
+        kit['info_text'] = info_text
+        print len(kits)-index-1, " remaining..."
+        minibrowser.close()
+    except:
+        pass
 
-with open('result.json', 'w') as fp:
+with open('results.json', 'w') as fp:
     json.dump(kits, fp)
 
 print "program finished successfully"
